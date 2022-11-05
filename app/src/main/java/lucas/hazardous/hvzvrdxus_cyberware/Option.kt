2604f4d69c2e0e38147f3b73d172a92d6e8517d6
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-11-05 17:42:22 +0100

    new option list layout

--- app/src/main/java/lucas/hazardous/hvzvrdxus_cyberware/Option.kt
+++ app/src/main/java/lucas/hazardous/hvzvrdxus_cyberware/Option.kt
@@ -1,9 +1,6 @@
 package lucas.hazardous.hvzvrdxus_cyberware
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
@@ -18,7 +15,7 @@ class Option(val text: String, val action: () -> Unit)
 @Composable
 fun OptionButton(option: Option) {
     Button(
-        modifier = Modifier.padding(vertical = 8.dp),
+        modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
         onClick = option.action
     ) {
         Text(option.text)
@@ -28,7 +25,7 @@ fun OptionButton(option: Option) {
 @Composable
 fun OptionsScreen(options: List<Option>) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.padding(48.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
