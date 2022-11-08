9bc9ea83cb1a5f2f4d0e0049347e40ced879ff89
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2022-11-08 16:11:30 +0100

    user list: space between text and button

--- app/src/main/java/lucas/hazardous/hvzvrdxus_cyberware/User.kt
+++ app/src/main/java/lucas/hazardous/hvzvrdxus_cyberware/User.kt
@@ -1,10 +1,7 @@
 package lucas.hazardous.hvzvrdxus_cyberware
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
@@ -87,7 +84,7 @@ fun UserElement(user: User) {
                 Text("Name: " + user.name)
                 Text("Surname: " + user.surname)
             }
-
+            Spacer(Modifier.weight(1f))
             OutlinedButton(
                 onClick = { ApiRequests.deleteUser(user.id); btnEnabled = false },
                 enabled = btnEnabled,
